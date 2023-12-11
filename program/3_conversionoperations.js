e8be5d2c33d149ad2f62dad06472976ec446754b
Santy0410 <santosh4102002@gmail.com>
2023-12-11 17:07:25 +0530

    updated

---
+++ program/3_conversionoperations.js
@@ -0,0 +1,28 @@
+// conversion//
+// number conversion
+let somevalue = "33"
+let numberedvalue = Number(somevalue)
+console.log(typeof (numberedvalue))
+console.log (numberedvalue)
+
+//"33ab" => nan
+//true = 1 ; false = 0;
+
+
+//boolean
+//syntax
+let value = 1
+let booleanvalue = Boolean(value)
+console.log(booleanvalue)
+
+//1 = true;
+// 0 = false ;
+//"" = false
+//"string" = true
+
+
+//string
+
+let stringname = 33
+let stringConverted = String(stringname)
+console.log(typeof stringConverted)
