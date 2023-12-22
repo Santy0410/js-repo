f8cc416716ee3bb05a4e1f34996b503b4f62258e
Santy0410 <santosh4102002@gmail.com>
2023-12-22 22:31:02 +0530

    object

---
+++ basics_2/4_objects.js
@@ -0,0 +1,27 @@
+const tinderUser = { }
+tinderUser.id = "1234"
+tinderUser.name = "sammy"
+
+const regularUser = {
+    email: "santy@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "santy",
+            lastname: "t"
+        }
+    }
+}
+// console.log(regularUser.fullname?.userfullname.lastname);
+const obj_1 = {  1:"a",2:"b"}
+const obj_2 = {  5:"a",4:"b"}
+
+// const obj_3 = Object.assign({},obj_1,obj_2);
+
+const obj_3 = {...obj_1,...obj_2}; 
+console.log(obj_3);
+
+console.log(Object.values(tinderUser));
+console.log(Object.keys(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("id"));
